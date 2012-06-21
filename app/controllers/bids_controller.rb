c1a4c38ea54380064e71a8118076941cc5c15403
Scott Fey <scottfey@gmail.com>
2012-06-20 22:23:34 -0500

    Enter minimum bid

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,14 +1,20 @@
 class BidsController < ApplicationController
 
   def create 
-    if params[:bid][:bid_amount].to_i < Team.find_by_id(params[:bid][:team_id]).bids.last.bid_amount 
-      flash[:alert] = "You must enter a bid higher than $#{Team.find_by_id(params[:bid][:team_id]).bids.last.bid_amount}"
-      redirect_to teams_url
-    else  
+    if Team.find_by_id(params[:bid][:team_id]).bids.present?
+      if params[:bid][:bid_amount].to_i < Team.find_by_id(params[:bid][:team_id]).bids.last.bid_amount 
+        flash[:alert] = "You must enter a bid higher than $#{Team.find_by_id(params[:bid][:team_id]).bids.last.bid_amount}"
+        redirect_to teams_url
+      else  
+        @bid = Bid.new(params[:bid])
+        @bid.save
+        redirect_to teams_url
+      end
+    else
       @bid = Bid.new(params[:bid])
       @bid.save
-      redirect_to teams_url
-    end
+      redirect_to teams_url          
+    end  
   end
   
   def new
